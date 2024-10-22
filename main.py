ffbd6be552ac16ec4098fe77a1a156f0e518c1fe
CS21B043 <cs21b043@iittp.ac.in>
2024-10-22 14:34:12 +0530

    Basic

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from Demo_24Oct24 import VM_Demo
+
+if __name__ == '__main__':
+    script_dir = os.path.dirname(__file__)
+    rel_path = sys.argv[1]
+    abs_file_path = os.path.join(script_dir, rel_path)
+    asm_code = ""
+
+    with open(abs_file_path) as file:
+        vm_code = file.read()
+
+        # asm_generator = VM()
+        print(vm_code)
+        asm_generator = VM_Demo()
+        # starting point of the virtual machine
+        asm_code = asm_generator.generate_target_code(vm_code)
+
+    script_dir = os.path.dirname(__file__)
+    rel_path = 'outputs/add.asm'
+    abs_file_path = os.path.join(script_dir, rel_path)
+
+    with open(abs_file_path, 'w+') as file:
+        file.write(asm_code)
--- preprocess.py
+++ preprocess.py
@@ -18,22 +18,27 @@ class Preprocess:
         # Neq
         mod_vm_code = ''
         for line in generator:
-            if(line.split(' ')[0] == 'function'):
-                if(line.split(' ')[2] == '0' and line.split(' ')[3] == '0'):
-                    line = line.split(' ')
-                    mod_vm_code += f'function {line[1]} 10 10 {line[4]}\n'
-                elif(line.split(' ')[2] == '0'):
-                    line = line.split(' ')
-                    mod_vm_code += f'function {line[1]} 10 {line[3]} {line[4]}\n'
-                elif(line.split(' ')[3] == '0'):
-                    line = line.split(' ')
-                    mod_vm_code += f'function {line[1]} {line[2]} 10 {line[4]}\n'
+            split_line = line.split(' ')
+
+            if len(split_line) == 0:
+                continue
+
+            if split_line[0] == 'function':
+                if len(split_line) < 5:
+                    # Add default values to handle missing elements
+                    split_line += ['0'] * (5 - len(split_line))
+
+                if split_line[2] == '0' and split_line[3] == '0':
+                    mod_vm_code += f'function {split_line[1]} 10 10 {split_line[4]}\n'
+                elif split_line[2] == '0':
+                    mod_vm_code += f'function {split_line[1]} 10 {split_line[3]} {split_line[4]}\n'
+                elif split_line[3] == '0':
+                    mod_vm_code += f'function {split_line[1]} {split_line[2]} 10 {split_line[4]}\n'
                 else:
                     mod_vm_code += line + '\n'
 
-            elif (line.split(' ')[0] == Instructions.Neq.value):
-                mod_vm_code += f"eq {line.split(' ')[-1]}\n"
-                # print(line)
+            elif split_line[0] == Instructions.Neq.value:
+                mod_vm_code += f"eq {split_line[-1]}\n"
                 line = next(generator)
                 label = self.get_new_label()
                 mod_vm_code += f"if-goto {label}\n"
@@ -41,27 +46,26 @@ class Preprocess:
                 prev_label = line.split(' ')[-1]
                 mod_vm_code += f"goto {prev_label}\n"
                 mod_vm_code += f"label {label}\n"
-                # print(label, prev_label)
-            elif (line.split(' ')[0] == Instructions.Not.value):
-                line = line.split(' ')
+
+            elif split_line[0] == Instructions.Not.value:
                 label0 = self.get_new_label()
                 label1 = self.get_new_label()
-                mod_vm_code += f"push constant 0 {line[-1]}\n"
-                mod_vm_code += f"eq {line[-1]}\n"
+                mod_vm_code += f"push constant 0 {split_line[-1]}\n"
+                mod_vm_code += f"eq {split_line[-1]}\n"
                 mod_vm_code += f"if-goto {label0}\n"
-                mod_vm_code += f"push constant 0 {line[-1]}\n"
+                mod_vm_code += f"push constant 0 {split_line[-1]}\n"
                 mod_vm_code += f"goto {label1}\n"
                 mod_vm_code += f"label {label0}\n"
-                mod_vm_code += f"push constant 1 {line[-1]}\n"
+                mod_vm_code += f"push constant 1 {split_line[-1]}\n"
                 mod_vm_code += f"label {label1}\n"
-            elif (';' in line):
-                mod_vm_code += re.sub(r';.*', '\n', line).strip()
-                mod_vm_code += '\n'
-            elif ('#' in line):
-                mod_vm_code += re.sub(r'#', '__', line)
-                mod_vm_code += '\n'
+
+            elif ';' in line:
+                mod_vm_code += re.sub(r';.*', '\n', line).strip() + '\n'
+
+            elif '#' in line:
+                mod_vm_code += re.sub(r'#', '__', line) + '\n'
+
             else:
                 mod_vm_code += line + '\n'
 
-        # print(mod_vm_code)
         return mod_vm_code
